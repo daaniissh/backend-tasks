896f4e42147514fe945d62a92997b41267de9f5b
Danish <pkdanish329@gmail.com>
2023-05-16 22:19:18 +0530

    first task completed

---
+++ index.js
@@ -0,0 +1,5 @@
+const { add, sub, divide } = require("./utlis");
+
+console.log(add(), "==add");
+console.log(sub(), "==sub");
+console.log(divide(), "==divide");
---
+++ utlis.js
@@ -0,0 +1,10 @@
+function add() {
+  return 10 + 10;
+}
+function sub() {
+  return 10 - 9;
+}
+function divide() {
+  return 100 * 5;
+}
+module.exports = { add, sub, divide };
